ba3af09524ce4e00699e49e2782a33260bf4313f
lalawue <suchaaa@gmail.com>
2017-05-07 15:13:45 +0800

    fix dir.

--- install_config.sh
+++ install_config.sh
@@ -2,10 +2,12 @@
 
 XCODE_USER_DATA_PATH=~/Library/Developer/Xcode/UserData
 SYSTEM_TYPE=$(uname)
+FONT_AND_COLOR_THEME_DIR=$XCODE_USER_DATA_PATH/FontAndColorThemes/
 
 echo "### Begin update ..."
 if [ $SYSTEM_TYPE == "Darwin" ]; then
 	cp -v emacs.idekeybindings "$XCODE_USER_DATA_PATH/KeyBindings/"
-	cp -v Default_C2.dvtcolortheme "$XCODE_USER_DATA_PATH/FontAndColorThemes/"
+	mkdir -p $FONT_AND_COLOR_THEME_DIR
+	cp -v Default_C2.dvtcolortheme "$FONT_AND_COLOR_THEME_DIR"
 fi
 echo "### End"
